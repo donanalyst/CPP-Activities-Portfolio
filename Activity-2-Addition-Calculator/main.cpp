967ecff47457af0cd10c1d3f3306b85a83c7a6e2
donanalyst <jervickamodia@gmail.com>
2026-02-03 21:07:27 +0800

    Added the line comments inside the CPP file

--- Activity-2-Addition-Calculator/main.cpp
+++ Activity-2-Addition-Calculator/main.cpp
@@ -7,13 +7,15 @@ int main() {
 
     int x, y;
 
-    cout << "Please enter the first number: ";
+    cout << "Please enter the first number: "; // ask user input
     cin >> x;
-    cout << "Pleae enter the second number: ";
+    cout << "Pleae enter the second number: "; // ask user for second input
     cin >> y;
 
     cout << "\n\n";
 
+
+    // Displaying the result
     cout << "=================================" << "\n";
     cout << "The sum of the two number is: " << x + y << endl;
     cout << "=================================" << endl;
